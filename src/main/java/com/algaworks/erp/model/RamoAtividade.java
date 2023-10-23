6df89d964826ee0d154a683a6590eba95e1b140d
eduardobnhr <eduardobnhr@gmail.com>
2023-10-23 11:04:23 -0400

    criando camada de persistencia do projeto

--- src/main/java/com/algaworks/erp/model/RamoAtividade.java
+++ src/main/java/com/algaworks/erp/model/RamoAtividade.java
@@ -2,7 +2,6 @@ package com.algaworks.erp.model;
 
 import java.io.Serializable;
 import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,7 +16,7 @@ public class RamoAtividade implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)//vai atribuir um valor para primaryLey
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//vai atribuir um valor para primaryKey
 	private Long id;
 	
 	@Column(nullable = false, length = 80)
@@ -38,7 +37,10 @@ public class RamoAtividade implements Serializable {
 	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -49,14 +51,15 @@ public class RamoAtividade implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		RamoAtividade other = (RamoAtividade) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	@Override
 	public String toString() {
 		return "RamoAtividade [id=" + id + "]";
-	}
-	
-	
-	
-
+	}	
 }
