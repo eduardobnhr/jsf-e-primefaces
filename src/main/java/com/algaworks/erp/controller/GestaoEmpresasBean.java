85ee9336d9ba150ff7d7bea6ff342fe091e06695
eduardobnhr <eduardobnhr@gmail.com>
2023-11-08 08:15:55 -0400

    criando o formulario para cadastro

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.model.TipoEmpresa;
 import com.algaworks.erp.repository.Empresas;
 import com.algaworks.erp.util.FacesMessages;
 
@@ -47,6 +48,10 @@ public class GestaoEmpresasBean implements Serializable{
 	}
 	public void setTermoPesquisa(String termoPesquisa) {
 		this.termoPesquisa = termoPesquisa;
+	}
+	
+	public TipoEmpresa[] getTiposEmpresa(){
+		return TipoEmpresa.values();
 	}
 
 }
