5c79f5821aaa1bcb5a1a3cba51018ffaa5598356
eduardobnhr <laraujo@digiboard.com.br>
2023-11-16 11:02:40 -0400

    ajustando o converter

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -17,60 +17,62 @@ import com.algaworks.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
-public class GestaoEmpresasBean implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	private List<Empresa> listaEmpresas;
-	
-	private String termoPesquisa;
-	
-	@Inject
-	private FacesMessages messages;
-	
-	@Inject
-	private RamoAtividades ramoAtividades;
-	
-	private Converter ramoAtividadeConverter;
-	
-	public void pesquisar() {
-		listaEmpresas = empresas.pesquisar(termoPesquisa);
-		
-		if(listaEmpresas.isEmpty()) {
-			messages.info("Sua consulta não retornou registros.");
-		}
-	}
-	
-	@Inject
-	private Empresas empresas;
-	
-	public void todasEmpresas() {
-		listaEmpresas = empresas.todas();
-	}
-	public List<RamoAtividade> completarRamoAtividade(String termo){
-		List<RamoAtividade> listaRamoAtividades = ramoAtividades.pesquisar(termo);
-		
-		ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
-		
-		return listaRamoAtividades;
-	}
-	
-	public List<Empresa> getListaEmpresas() {
-		return listaEmpresas;
-	}
-	
-	public String getTermoPesquisa() {
-		return termoPesquisa;
-	}
-	public void setTermoPesquisa(String termoPesquisa) {
-		this.termoPesquisa = termoPesquisa;
-	}
-	
-	public TipoEmpresa[] getTiposEmpresa(){
-		return TipoEmpresa.values();
-	}
-	
-	public Converter getRamoAtividadeConverter() {
-		return ramoAtividadeConverter;
-	}
+public class GestaoEmpresasBean implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private Empresas empresas;
+    
+    @Inject
+    private FacesMessages messages;
+    
+    @Inject
+    private RamoAtividades ramoAtividades;
+    
+    private List<Empresa> listaEmpresas;
+    
+    private String termoPesquisa;
+    
+    private Converter ramoAtividadeConverter;
+    
+    public void pesquisar() {
+        listaEmpresas = empresas.pesquisar(termoPesquisa);
+        
+        if (listaEmpresas.isEmpty()) {
+            messages.info("Sua consulta não retornou registros.");
+        }
+    }
+    
+    public void todasEmpresas() {
+        listaEmpresas = empresas.todas();
+    }
+    
+    public List<RamoAtividade> completarRamoAtividade(String termo) {
+        List<RamoAtividade> listaRamoAtividades = ramoAtividades.pesquisar(termo);
+        
+        ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
+        
+        return listaRamoAtividades;
+    }
+    
+    public List<Empresa> getListaEmpresas() {
+        return listaEmpresas;
+    }
+    
+    public String getTermoPesquisa() {
+        return termoPesquisa;
+    }
+    
+    public void setTermoPesquisa(String termoPesquisa) {
+        this.termoPesquisa = termoPesquisa;
+    }
+    
+    public TipoEmpresa[] getTiposEmpresa() {
+        return TipoEmpresa.values();
+    }
+    
+    public Converter getRamoAtividadeConverter() {
+        return ramoAtividadeConverter;
+    }
 }
