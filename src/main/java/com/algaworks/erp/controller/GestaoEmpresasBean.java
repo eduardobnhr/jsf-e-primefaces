ff377b428b38e7d3a540cefdc35c8dcb990f8fb3
eduardobnhr <eduardobnhr@gmail.com>
2023-11-16 12:22:44 -0400

    implementando o cadastro de empresa

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -13,6 +13,7 @@ import com.algaworks.erp.model.RamoAtividade;
 import com.algaworks.erp.model.TipoEmpresa;
 import com.algaworks.erp.repository.Empresas;
 import com.algaworks.erp.repository.RamoAtividades;
+import com.algaworks.erp.service.CadastroEmpresaService;
 import com.algaworks.erp.util.FacesMessages;
 
 @Named
@@ -30,11 +31,30 @@ public class GestaoEmpresasBean implements Serializable {
     @Inject
     private RamoAtividades ramoAtividades;
     
+    @Inject
+    private CadastroEmpresaService cadastroEmpresaService;
+    
     private List<Empresa> listaEmpresas;
     
     private String termoPesquisa;
     
     private Converter ramoAtividadeConverter;
+    
+    private Empresa empresa;
+    
+    public void prepararNovaEmpresa() {
+    	empresa = new Empresa();
+    }
+    
+    public void salvar() {
+    	cadastroEmpresaService.salvar(empresa);
+    	
+    	if(jaHouvePesquisa()) {
+    		pesquisar();
+    	}
+    	messages.info("Empresa cadastrada com sucesso!");
+    	
+    }
     
     public void pesquisar() {
         listaEmpresas = empresas.pesquisar(termoPesquisa);
@@ -57,6 +77,10 @@ public class GestaoEmpresasBean implements Serializable {
         return listaRamoAtividades;
     }
     
+    private boolean jaHouvePesquisa() {
+    	return termoPesquisa != null && !"".equals(termoPesquisa);
+    }
+    
     public List<Empresa> getListaEmpresas() {
         return listaEmpresas;
     }
@@ -76,4 +100,7 @@ public class GestaoEmpresasBean implements Serializable {
     public Converter getRamoAtividadeConverter() {
         return ramoAtividadeConverter;
     }
+    public Empresa getEmpresa() {
+		return empresa;
+	}
 }
